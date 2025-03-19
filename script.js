027ca699453842c654fe8b5b3acb33e7af4196db
Recep Barkın Topcu <101873638+BarkinTopcu@users.noreply.github.com>
2025-03-20 02:43:03 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -16,3 +16,33 @@ searchInput.addEventListener('keydown', (e) => {
     }
   });
   
+
+  const popular_btn = document.getElementById('popular_btn');
+  const recent_btn = document.getElementById('recent_btn');
+  const pop_div_area = document.getElementById('pop_div_area');
+  
+  popular_btn.addEventListener('click', () => {
+    popular_btn.classList.add('active');
+    recent_btn.classList.remove('active');
+    pop_div_area.innerHTML = `
+      <h3>En Çok Tıklanan Makaleler</h3>
+      <ol>
+        <li>Makale 1</li>
+        <li>Makale 2</li>
+        <li>Makale 3</li>
+      </ol>
+    `;
+  });
+  
+  recent_btn.addEventListener('click', () => {
+    recent_btn.classList.add('active');
+    popular_btn.classList.remove('active');
+    pop_div_area.innerHTML = `
+      <h3>En Son Yüklenen Makaleler</h3>
+      <ol>
+        <li>Makale 1</li>
+        <li>Makale 2</li>
+        <li>Makale 3</li>
+      </ol>
+    `;
+  });
